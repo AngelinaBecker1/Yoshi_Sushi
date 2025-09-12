ef9a768071d65e0677c431cd447070ce0533490f
AngelinaBecker1 <angelina.becker@escola.pr.gov.br>
2025-09-12 12:27:23 -0300

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,20 @@
+function openmenu() {
+    document.getElementById("menu").style.width = "280px";
+    document.querySelector(".tres-barrinhas").style.opacity = "0";
+    document.body.style.overflow = "hidden";
+}
 
+function closemenu() {
+    document.getElementById("menu").style.width = "0";
+    document.querySelector(".tres-barrinhas").style.opacity = "1";
+    document.body.style.overflow = "auto";
+}
+
+document.addEventListener('click', function (event) {
+    const menu = document.getElementById('menu');
+    const btn = document.querySelector('.tres-barrinhas');
+
+    if (!menu.contains(event.target) && event.target !== btn && !btn.contains(event.target)) {
+        closemenu();
+    }
+});
